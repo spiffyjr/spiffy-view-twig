58b26287c7e5a241b66580a29af15496fd724867
Kyle Spraggs <theman@spiffyjr.me>
2014-04-06 14:10:42 -0500

    Initial commit

---
+++ src/TwigRenderer.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Spiffy\View;
+
+class TwigRenderer implements Renderer
+{
+    /**
+     * @var TwigResolver
+     */
+    protected $resolver;
+
+    /**
+     * @var \Twig_Environment
+     */
+    protected $twig;
+
+    /**
+     * @param \Twig_Environment $twig
+     */
+    public function __construct(\Twig_Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getEngine()
+    {
+        return $this->twig;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setResolver(Resolver $resolver)
+    {
+        $this->resolver = $resolver;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function render($nameOrModel, array $variables = [])
+    {
+        if (!$nameOrModel instanceof ViewModel) {
+            $model = new ViewModel();
+            $model->setVariables($variables);
+            $model->setTemplate($nameOrModel);
+
+            $nameOrModel = $model;
+        }
+        $template = $this->resolver->resolve($nameOrModel);
+
+        return $template->render($nameOrModel->getVariables());
+    }
+}
---
+++ test/TwigRendererTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Spiffy\View;
+
+/**
+ * @coversDefaultClass \Spiffy\View\TwigRenderer
+ */
+class TwigRendererTest extends \PHPUnit_Framework_TestCase
+{
+
+}
